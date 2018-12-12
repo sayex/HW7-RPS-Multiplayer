a0213d4885c4a8904cee588e27a3b4b881393d67
sayex <elsayer@msn.com>
2018-12-11 18:40:46 -0700

    firebase setup

---
+++ assets/javascript/logic.js
@@ -0,0 +1,16 @@
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyDTBQ1ZXVvtIPmtMWD0jCJ4Q0yG1ZGFS64",
+    authDomain: "rock-paper-scissors-20c47.firebaseapp.com",
+    databaseURL: "https://rock-paper-scissors-20c47.firebaseio.com",
+    projectId: "rock-paper-scissors-20c47",
+    storageBucket: "rock-paper-scissors-20c47.appspot.com",
+    messagingSenderId: "109051506827"
+};
+firebase.initializeApp(config);
+
+
+$(document).ready({
+
+
+})

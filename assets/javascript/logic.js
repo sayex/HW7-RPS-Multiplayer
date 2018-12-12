7f68c5ce709733fe3b6d790a1632f449fe762f1f
sayex <elsayer@msn.com>
2018-12-11 22:46:51 -0700

    win loss logic is worknig and screen updates are working

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -1,29 +1,212 @@
-// Initialize Firebase
-var config = {
-    apiKey: "AIzaSyDTBQ1ZXVvtIPmtMWD0jCJ4Q0yG1ZGFS64",
-    authDomain: "rock-paper-scissors-20c47.firebaseapp.com",
-    databaseURL: "https://rock-paper-scissors-20c47.firebaseio.com",
-    projectId: "rock-paper-scissors-20c47",
-    storageBucket: "rock-paper-scissors-20c47.appspot.com",
-    messagingSenderId: "109051506827"
-};
-firebase.initializeApp(config);
+$(document).ready(function () {
+    // Initialize Firebase
+    var config = {
+        apiKey: "AIzaSyDTBQ1ZXVvtIPmtMWD0jCJ4Q0yG1ZGFS64",
+        authDomain: "rock-paper-scissors-20c47.firebaseapp.com",
+        databaseURL: "https://rock-paper-scissors-20c47.firebaseio.com",
+        projectId: "rock-paper-scissors-20c47",
+        storageBucket: "rock-paper-scissors-20c47.appspot.com",
+        messagingSenderId: "109051506827"
+    };
+    firebase.initializeApp(config);
 
 
-//create varables
+    //create varables
 
-var player1Wins = 0
-var plaery2Wins = 0
-var player1Losses = 0
-var player2Losses = 0
-var ties = 0
+    var player1Wins = 0;
+    var player2Wins = 0;
+    var player1Losses = 0;
+    var player2Losses = 0;
+    var ties = 0;
+    var player1Set = false;
+    var player1Name = ""
+    var player2Set = false;
+    var player2Name = "";
+    var player1Choice = "";
+    var player2Choice = "";
+    var player1ChoiceSet = false;
+    var player2ChoiceSet = false;
 
-//function go grab user name input
 
-//function to grap button click for player 1
 
-//function to grab button clicks for player 2
+    //function go grab user name input
+    function playerSetup() {
+        $(".player1").hide();
+        $(".player2").hide();
+        $("#submit").on("click", function () {
+            event.preventDefault();
+            var inputName = $("#inputName").val();
+            $("#inputName").val("")
+            if (!player1Set) {
+                player1Set = true
+                player1Name = inputName
+                $("#player1Name").text(player1Name);
+                $(".player1").show();
+            } else if (player1Set && !player2Set) {
+                player2Set = true
+                player2Name = inputName
+                $("#player2Name").text(player2Name);
+                $(".player2").show();
+            }
+        })
+    }
 
-//function to update screen with user clicks
 
-//function to update screen with winner and update user wins losses and ties
+    //function to grap button click for player 1 and 2
+
+    $("button").on("click", function () {
+        var player = $(this).attr("data-player")
+        var selectedButton = $(this).text()
+        if (player === "player1") {
+            player1Choice = selectedButton
+
+            $(".player1").hide();
+            player1ChoiceSet = true
+        } else if (player === "player2") {
+            player2Choice = selectedButton
+
+            $(".player2").hide();
+            player2ChoiceSet = true
+
+        }
+        if (player1ChoiceSet === true && player2ChoiceSet === true) {
+            $("#player1Choice").text(player1Choice);
+            $("#player2Choice").text(player2Choice);
+            RPSLogic()
+
+        }
+    })
+
+    //function to update screen with user clicks
+
+    function RPSLogic() {
+        if (player1Choice === "Rock" && player2Choice === "Paper") {
+            $("#textReults").text("Paper Covers Rock");
+            $("#playerWins").text(player2Name + " Wins");
+            player2Wins++
+            player1Losses++
+            scoreUpdate()
+            setTimeout(function () {
+                gameReset()
+            }, 3000)
+        }
+
+        if (player1Choice === "Paper" && player2Choice === "Scissors") {
+            $("#textReults").text("Scissors Cuts Paper");
+            $("#playerWins").text(player2Name + " Wins");
+            player2Wins++
+            player1Losses++
+            scoreUpdate()
+            setTimeout(function () {
+                gameReset()
+            }, 3000)
+        }
+
+        if (player1Choice === "Scissors" && player2Choice === "Rock") {
+            $("#textReults").text("Rock Smashes Scissors");
+            $("#playerWins").text(player2Name + " Wins");
+            player2Wins++
+            player1Losses++
+            scoreUpdate()
+            setTimeout(function () {
+                gameReset()
+            }, 3000)
+
+        }
+        if (player1Choice === "Rock" && player2Choice === "Scissors") {
+            $("#textReults").text("Rock Smashes Scissors");
+            $("#playerWins").text(player1Name + " Wins");
+            player1Wins++
+            player2Losses++
+            scoreUpdate()
+            setTimeout(function () {
+                gameReset()
+            }, 3000)
+        }
+
+        if (player1Choice === "Paper" && player2Choice === "Rock") {
+            $("#textReults").text("Paper Covers Rock");
+            $("#playerWins").text(player1Name + " Wins");
+            player1Wins++
+            player2Losses++
+            scoreUpdate()
+            setTimeout(function () {
+                gameReset()
+            }, 3000)
+        }
+
+        if (player1Choice === "Scissors" && player2Choice === "Paper") {
+            $("#textReults").text("Scissors Cuts Paper");
+            $("#playerWins").text(player1Name + " Wins");
+            player1Wins++
+            player2Losses++
+            scoreUpdate()
+            setTimeout(function () {
+                gameReset()
+            }, 3000)
+
+        }
+
+        if (player1Choice === "Rock" && player2Choice === "Rock") {
+            $("#textReults").text("Tie");
+            $("#playerWins").text("");
+            ties++
+            scoreUpdate()
+            setTimeout(function () {
+                gameReset()
+            }, 3000)
+
+        }
+
+        if (player1Choice === "Paper" && player2Choice === "Paper") {
+            $("#textReults").text("Tie");
+            $("#playerWins").text("");
+            ties++
+            scoreUpdate()
+            setTimeout(function () {
+                gameReset()
+            }, 3000)
+        }
+
+        if (player1Choice === "Scissors" && player2Choice === "Scissors") {
+            $("#textReults").text("Tie");
+            $("#playerWins").text("");
+            ties++
+            scoreUpdate()
+            setTimeout(function () {
+                gameReset()
+            }, 3000)
+
+        }
+    }
+
+    //function to update screen with winner and update user wins losses and ties
+
+    function scoreUpdate() {
+        $("#wins1").text(player1Wins);
+        $("#loss1").text(player1Losses);
+        $("#wins2").text(player2Wins);
+        $("#loss2").text(player2Losses);
+        $("#ties1").text(ties)
+        $("#ties2").text(ties)
+    }
+
+    function gameReset() {
+        $("#player1Chose").text("");
+        $("#player2Chose").attr("data-choice", "");
+        $(".player1").show();
+        $("#player2Chose").text("");
+        $("#player2Chose").attr("data-choice", "");
+        $(".player2").show();
+        $("#testReults").text("");
+        $("#playerWins").text("");
+        player1ChoiceSet = false
+        player2ChoiceSet = false
+
+    }
+
+    playerSetup()
+    scoreUpdate()
+
+
+})

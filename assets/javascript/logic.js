e4f678d57fde57560246d461eb75e9728aaa30ed
sayex <elsayer@msn.com>
2018-12-11 19:31:55 -0700

    seting up html added sticky footer and created two cards for player selected

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -10,7 +10,20 @@ var config = {
 firebase.initializeApp(config);
 
 
-$(document).ready({
+//create varables
 
+var player1Wins = 0
+var plaery2Wins = 0
+var player1Losses = 0
+var player2Losses = 0
+var ties = 0
 
-})
+//function go grab user name input
+
+//function to grap button click for player 1
+
+//function to grab button clicks for player 2
+
+//function to update screen with user clicks
+
+//function to update screen with winner and update user wins losses and ties
